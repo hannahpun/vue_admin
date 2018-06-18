100bb72a4f366e98b37e8a49b273393312191425
hannahpun <hannahpun@gmail.com>
2018-06-18 17:47:13 +0800

    init store

--- src/main.js
+++ src/main.js
@@ -3,13 +3,27 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import ElementUI from 'element-ui'
+import underscore from 'vue-underscore'
+
+import store from './store.js'
+
+import './styles/element-variables.scss'
+import './styles/application.scss'
 
 Vue.config.productionTip = false
+
+Vue.use(ElementUI)
+Vue.use(VueAxios, axios)
+Vue.use(underscore)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
