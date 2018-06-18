239c05e1581fdf09e8785d5710a78046ed11ad84
hannahpun <hannahpun@gmail.com>
2018-06-18 17:42:12 +0800

    add styles init

--- build/utils.js
+++ build/utils.js
@@ -57,12 +57,13 @@ exports.cssLoaders = function (options) {
   // https://vue-loader.vuejs.org/en/configurations/extract-css.html
   return {
     css: generateLoaders(),
-    postcss: generateLoaders(),
-    less: generateLoaders('less'),
-    sass: generateLoaders('sass', { indentedSyntax: true }),
-    scss: generateLoaders('sass'),
-    stylus: generateLoaders('stylus'),
-    styl: generateLoaders('stylus')
+    scss: generateLoaders('sass').concat(
+    {
+      loader: 'sass-resources-loader',
+      options: {
+        resources: [path.resolve(__dirname, '../src/styles/_variables.scss'), path.resolve(__dirname, '../src/styles/_helpers.scss')]
+      }
+    })
   }
 }
 
